c0dabb8a1ab5dc503695c5ce7f72de56c77c6c55
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-03-25 12:42:41 -0500

    RSPM

--- scripts/install_r.sh
+++ scripts/install_r.sh
@@ -16,6 +16,9 @@ apt-get install -y gdebi-core
 # R Package Dependencies
 apt-get install -y build-essential libcurl4-gnutls-dev openjdk-7-* libxml2-dev libssl-dev texlive-full
 
-curl -o /tmp/r-${R_VERSION}_1_amd64.deb https://cdn.rstudio.com/r/ubuntu-1804/pkgs/r-${R_VERSION}_1_amd64.deb
-gdebi --non-interactive /tmp/r-${R_VERSION}_1_amd64.deb
-rm /tmp/r-${R_VERSION}_1_amd64.deb
+for R_VER in ${R_VERSION}
+do
+    curl -o /tmp/r-${R_VER}_1_amd64.deb https://cdn.rstudio.com/r/ubuntu-1804/pkgs/r-${R_VER}_1_amd64.deb
+    gdebi --non-interactive /tmp/r-${R_VER}_1_amd64.deb
+    rm /tmp/r-${R_VER}_1_amd64.deb
+done
