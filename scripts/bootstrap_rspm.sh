cc87ee6c3551203550cf6c24919d6d55150873b0
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-03-26 11:09:02 -0500

    Update

--- scripts/bootstrap_rspm.sh
+++ scripts/bootstrap_rspm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 set -xe
-
-exec > >(tee /var/log/bootstrap.log | logger -t bootstrap -s 2>/dev/console) 2>&1
 
 export RSPM_VERSION=$1
 export R_VERSION=$2
