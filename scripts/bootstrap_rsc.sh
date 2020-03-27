ed86128bbe5ddc81da5b9fb614d98ae8d7f91261
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-03-26 22:50:02 -0500

    Update

--- scripts/bootstrap_rsc.sh
+++ scripts/bootstrap_rsc.sh
@@ -4,12 +4,14 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 set -xe
 
 export RSC_VERSION=$1
-export R_VERSION=$2
-export PYTHON_VERSION=$3
-export ANACONDA_VERSION=$4
-export DRIVERS_VERSION=$5
+export USERNAME=$2
+export PASSWORD=$3
+export R_VERSION=$4
+export PYTHON_VERSION=$5
+export ANACONDA_VERSION=$6
+export DRIVERS_VERSION=$7
 
 R_VERSION=$R_VERSION bash ./install_r.sh
 PYTHON_VERSION=$PYTHON_VERSION ANACONDA_VERSION=$ANACONDA_VERSION bash ./install_python.sh
 DRIVERS_VERSION=$DRIVERS_VERSION bash ./install_drivers.sh
-RSC_VERSION=$RSC_VERSION bash ./install_rsc.sh
+RSC_VERSION=$RSC_VERSION PASSWORD=$PASSWORD bash ./install_rsc.sh
