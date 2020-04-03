411ea81d689b7165d4a04f07bdb0e8f6ff461b15
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-04-03 16:06:13 -0500

    Update RSPM

--- scripts/bootstrap_rsc.sh
+++ scripts/bootstrap_rsc.sh
@@ -4,15 +4,15 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 set -xe
 
 export RSC_VERSION=${RSC_VERSION:-3.7.3 2.7.16}
-export USERNAME=${USERNAME:-3.7.3 2.7.16}
-export PASSWORD=${PASSWORD:-3.7.3 2.7.16}
+export RSC_USERNAME=${RSC_USERNAME:-3.7.3 2.7.16}
+export RSC_PASSWORD=${RSC_PASSWORD:-3.7.3 2.7.16}
 export R_VERSION=${R_VERSION:-3.7.3 2.7.16}
 export PYTHON_VERSION=${PYTHON_VERSION:-3.7.3 2.7.16}
 export ANACONDA_VERSION=${ANACONDA_VERSION:-3.7.3 2.7.16}
 export DRIVERS_VERSION=${DRIVERS_VERSION:-3.7.3 2.7.16}
-export RSC_VERSION=${RSC_VERSION:-3.7.3 2.7.16}
+export RSPM_URL=${RSPM_URL:-3.7.3 2.7.16}
 
-R_VERSION=$R_VERSION bash ./install_r.sh
-PYTHON_VERSION=$PYTHON_VERSION ANACONDA_VERSION=$ANACONDA_VERSION bash ./install_python.sh
-DRIVERS_VERSION=$DRIVERS_VERSION bash ./install_drivers.sh
-RSC_VERSION=$RSC_VERSION USER=$USERNAME PASSWORD=$PASSWORD bash ./install_rsc.sh
+bash ./install_r.sh
+bash ./install_python.sh
+bash ./install_drivers.sh
+bash ./install_rsc.sh
--- scripts/bootstrap_rspm.sh
+++ scripts/bootstrap_rspm.sh
@@ -6,5 +6,5 @@ set -xe
 export RSPM_VERSION=${RSPM_VERSION:-1.1.2-10}
 export R_VERSION=${R_VERSION:-3.6.3}
 
-R_VERSION=$R_VERSION bash ./install_r.sh
-RSPM_VERSION=$RSPM_VERSION bash ./install_rspm.sh
+bash ./install_r.sh
+bash ./install_rspm.sh
