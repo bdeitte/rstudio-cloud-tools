81b7043fa4af873e36ce7aba6f5ea5718c2b33b6
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-04-05 15:09:18 -0500

    Fix variables

--- scripts/install_rsc.sh
+++ scripts/install_rsc.sh
@@ -62,4 +62,4 @@ systemctl restart rstudio-connect
 bash ./wait-for-it.sh localhost:80 -t 60
 
 # Create connect admin user
-curl -i -X POST -d "{\"email\": \"rstudio@example.com\", \"username\": \"${USER}\", \"password\": \"${PASSWORD}\"}" localhost:80/__api__/v1/users
+curl -i -X POST -d "{\"email\": \"rstudio@example.com\", \"username\": \"${RSC_USERNAME}\", \"password\": \"${RSC_PASSWORD}\"}" localhost:80/__api__/v1/users
