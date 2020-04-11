f59eebad71f111703d8133d06038a169d2194dc8
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-04-10 19:22:17 -0500

    Update

--- scripts/bootstrap_rsc.sh
+++ scripts/bootstrap_rsc.sh
@@ -3,11 +3,11 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 set -xe
 
 export RSC_VERSION=${RSC_VERSION:-1.8.2-10}
-export RSC_USERNAME=${RSC_USERNAME:-rstudio-user}
+export RSC_USERNAME=${RSC_USERNAME:-admin}
 export RSC_PASSWORD=${RSC_PASSWORD:-rstudio}
-export R_VERSION=${R_VERSION:-3.6.3}
-export PYTHON_VERSION=${PYTHON_VERSION:-3.7.3}
-export ANACONDA_VERSION=${ANACONDA_VERSION:-Miniconda3-4.7.10}
+export R_VERSIONS=${R_VERSIONS:-3.6.3}
+export PYTHON_VERSIONS=${PYTHON_VERSIONS:-3.7.3}
+export ANACONDA_VERSIONS=${ANACONDA_VERSIONS:-Miniconda3-4.7.10}
 export DRIVERS_VERSION=${DRIVERS_VERSION:-1.6.1}
 export RSPM_ADDRESS=${RSPM_ADDRESS}
 
