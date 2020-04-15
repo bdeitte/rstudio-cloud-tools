eb5d9908fed3119f74cb4c31d75ebfac65b99764
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-04-15 12:15:30 -0500

    Update RSPM

--- scripts/bootstrap_rspm.sh
+++ scripts/bootstrap_rspm.sh
@@ -2,7 +2,7 @@
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 set -xe
 
-export RSPM_VERSION=${RSPM_VERSION:-1.1.2-10}
+export RSPM_VERSION=${RSPM_VERSION:-1.1.4-3}
 export R_VERSION=${R_VERSION:-3.6.3}
 
 
