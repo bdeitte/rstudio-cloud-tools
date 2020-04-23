380530531602cbaf47bf965bde975d98dd861007
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-04-23 15:47:31 -0500

    No chown

--- scripts/az_data_disk.sh
+++ scripts/az_data_disk.sh
@@ -19,5 +19,5 @@ function partition_disk {
 mkdir -p $DISK_MNT
 lsblk -no FSTYPE $DISK_NAME | grep ext4 || partition_disk
 mount $DISK_PART $DISK_MNT
-chown $MNT_USER:$MNT_GROUP $DISK_MNT
+# chown $MNT_USER:$MNT_GROUP $DISK_MNT
 echo "$DISK_PART $DISK_MNT ext4 defaults 0 0" >> /etc/fstab
